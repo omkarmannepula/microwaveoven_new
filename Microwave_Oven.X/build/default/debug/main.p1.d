f41cfc47ba895d8d9d4074dbe8e8ade87d99818e
omkarmannepula <omkarofficial177@gmail.com>
2023-10-07 12:39:22 +0530

    Added Project to github

---
+++ Microwave_Oven.X/build/default/debug/main.p1.d
@@ -0,0 +1,6 @@
+build/default/debug/main.p1:  \
+main.c  \
+clcd.h  \
+main.h  \
+matrix_keypad.h  \
+timers.h 
---
+++ Microwave_Oven.X/build/default/debug/matrix_kaypad.p1.d
@@ -0,0 +1,3 @@
+build/default/debug/matrix_kaypad.p1:  \
+matrix_kaypad.c  \
+matrix_keypad.h 
